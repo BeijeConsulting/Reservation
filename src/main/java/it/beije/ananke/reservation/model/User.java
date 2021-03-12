3f08c5bac1d46860496f5208dd6c3593526ea935
Padawan08 <Padawan08@DESKTOP-57NL767.wind3.hub>
2021-03-12 17:58:43 +0100

    .

--- src/main/java/it/beije/ananke/reservation/model/User.java
+++ src/main/java/it/beije/ananke/reservation/model/User.java
@@ -1,5 +1,79 @@
 package it.beije.ananke.reservation.model;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="user")
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	Integer userId;
+	@Column(name="user_email")
+	String userEmail;
+	@Column(name="first_name")
+	String firstName;
+	@Column(name="last_name")
+	String lastName;
+	@Column(name="codice_fiscale")
+	String codiceFiscale;
+	@Column(name="password")
+	String password;
+	@Column(name="company_id")
+	Integer companyId;
+	@Column(name="vat_number")
+	Integer vatNumber;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+	public Integer getVatNumber() {
+		return vatNumber;
+	}
+	public void setVatNumber(Integer vatNumber) {
+		this.vatNumber = vatNumber;
+	}
 }
