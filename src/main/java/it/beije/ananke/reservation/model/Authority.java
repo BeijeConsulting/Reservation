537d81836cbe276f329796530d080ba9ad7a9330
Davide Persico <d.persico1@campus.unimib.it>
2021-03-12 17:52:18 +0100

    upload entities: address, authority, company

---
+++ src/main/java/it/beije/ananke/reservation/model/Authority.java
@@ -0,0 +1,38 @@
+package it.beije.ananke.reservation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="authority")
+public class Authority {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;
+	
+	@Column(name="authority_name")
+	private String authority;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	
+}
