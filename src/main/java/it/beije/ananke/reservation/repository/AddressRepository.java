b03e17050abf744583bc082cfd064da436ca24fe
Davide Persico <d.persico1@campus.unimib.it>
2021-03-15 14:57:32 +0100

    address service

--- src/main/java/it/beije/ananke/reservation/repository/AddressRepository.java
+++ src/main/java/it/beije/ananke/reservation/repository/AddressRepository.java
@@ -8,5 +8,10 @@ import it.beije.ananke.reservation.model.Address;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer>{
 
+	public Address findByUserId(Integer userId);
+
+	public Address findByCompanyId(Integer companyId);
+
+	public Address findByBuildingId(Integer buildingId);
 	
 }
--- src/main/java/it/beije/ananke/reservation/service/AddressService.java
+++ src/main/java/it/beije/ananke/reservation/service/AddressService.java
@@ -1,8 +1,52 @@
 package it.beije.ananke.reservation.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.beije.ananke.reservation.model.Address;
+import it.beije.ananke.reservation.repository.AddressRepository;
 
 @Service
 public class AddressService {
-
+	
+	@Autowired
+	private AddressRepository addressRepository;
+	
+	
+	public Address insertAddress(Address address) {	
+		
+		return addressRepository.save(address);
+		
+	}
+	
+	public Address updateAddress(Address address) {
+		
+		return addressRepository.saveAndFlush(address);
+		
+	}
+	
+	public void deleteAddress(Integer addressId) {
+		
+		addressRepository.deleteById(addressId);
+		
+	}
+	
+	public Address findByUserId(Integer userId) {
+		
+		return addressRepository.findByUserId(userId);
+		
+	}
+	
+	public Address findByCompanyId(Integer companyId) {
+		
+		return addressRepository.findByCompanyId(companyId);
+		
+	}
+	
+	public Address findByBuildingId(Integer buildingId) {
+		
+		return addressRepository.findByBuildingId(buildingId);
+		
+	}
+	
 }
