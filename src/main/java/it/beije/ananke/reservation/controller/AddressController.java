aabac3264a011f1fa8bbecfc3743f20298983eed
faheemjan5000 <faheemjan500@gmail.com>
2021-03-15 14:35:33 +0100

    controller classes added

--- src/main/java/it/beije/ananke/reservation/controller/AddressController.java
+++ src/main/java/it/beije/ananke/reservation/controller/AddressController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
-public class UserController {
-	
+@RequestMapping("/address")
+public class AddressController {
 
 }
