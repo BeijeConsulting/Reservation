60ebeb536a2ce9dcebaac4b874c632ebf28b41c3
faheemjan5000 <faheemjan500@gmail.com>
2021-03-23 15:30:42 +0100

    added controller,service and repository methods for Invoice.

--- src/main/java/it/beije/ananke/reservation/service/InvoiceService.java
+++ src/main/java/it/beije/ananke/reservation/service/InvoiceService.java
@@ -1,7 +1,27 @@
 package it.beije.ananke.reservation.service;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.beije.ananke.reservation.model.Invoice;
+import it.beije.ananke.reservation.repository.InvoiceRepository;
 
 @Service
 public class InvoiceService {
 
+	@Autowired
+	private InvoiceRepository invoiceRepository;
+	
+	public Invoice getInvoiceById(Integer id) {
+		return invoiceRepository.findByInvoiceId(id);
+	}
+	
+	public List<Invoice> getAllInvoices(){
+		return invoiceRepository.findAll();
+	}
+	
+	public void removeInvoice(Integer id) {
+		invoiceRepository.deleteById(id);
+	}
 }
