a393bb47ea272cbfd6bff999184d940d512af0f3
Davide Persico <d.persico1@campus.unimib.it>
2021-03-23 09:55:25 +0100

    update reservation controller

--- src/main/java/it/beije/ananke/reservation/controller/ReservationController.java
+++ src/main/java/it/beije/ananke/reservation/controller/ReservationController.java
@@ -7,9 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.beije.ananke.reservation.model.Reservation;
@@ -22,12 +21,12 @@ public class ReservationController {
   
 	@Autowired
 	private UserService userService;
+	
 	@Autowired
 	private ReservationService reservationService;
 
 	@PreAuthorize("hasAuthority('ADMIN')")
-	@RequestMapping(value="/test", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
+	@GetMapping("/test")
 	public String getCurrentUser(HttpServletRequest req){
 		
 		Principal principal = req.getUserPrincipal();
@@ -39,8 +38,7 @@ public class ReservationController {
 	}
 	
 	@PreAuthorize("hasAuthority('CUSTOMER')")
-	@RequestMapping(value="/list", method = RequestMethod.GET, produces = "application/json")
-	@ResponseBody
+	@GetMapping("/list")
 	public List<Reservation> getReservationList(HttpServletRequest req){
 		
 		Integer id = userService.findUserByUserEmail(req);
