65c8c2936026414069b51c0ea41ac240f6673a96
Davide Persico <d.persico1@campus.unimib.it>
2021-03-23 15:07:08 +0100

    update reservation controller, service

--- src/main/java/it/beije/ananke/reservation/controller/ReservationController.java
+++ src/main/java/it/beije/ananke/reservation/controller/ReservationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -89,7 +90,7 @@ public class ReservationController {
 	}
 	
 	@PreAuthorize("hasAuthority('CUSTOMER')")
-	@DeleteMapping("/update/{reservationId}")
+	@PutMapping("/update/{reservationId}")
 	public Reservation updateReservation(@PathVariable Integer reservationId, @RequestBody Reservation reservation) {
 		
 		return reservationService.updateReservation(reservation, reservationId);
